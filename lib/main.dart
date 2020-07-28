d9e7db65a2825ab81c08117bcbc63e62d3e9cd82
Greg <gregory.rolwes@slu.edu>
2020-07-27 20:21:32 -0500

    Testing connection to firebase
    Adds document to 'tasks' collection on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,6 +9,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Firestore.instance
+        .collection('tasks')
+        .document()
+        .setData({'title': 'title', 'author': 'author'});
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
